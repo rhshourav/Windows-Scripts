2b073c5d221e1d52bc3e86f912c7608464e8cdff
MD Shourav Hossain <89313694+rhshourav@users.noreply.github.com>
2025-12-24 17:51:19 +0600

    Update run.cmd

--- ERP-Automate/run.cmd
+++ ERP-Automate/run.cmd
@@ -1,4 +1,8 @@
 @echo off
-powershell -NoLogo -ExecutionPolicy Bypass -Command ^
-  "try { irm 'https://raw.githubusercontent.com/rhshourav/Windows-Scripts/refs/heads/main/ERP-Automate/run_Auto-ERP.ps1' | iex } ^
-   catch { Invoke-RestMethod 'https://raw.githubusercontent.com/rhshourav/Windows-Scripts/refs/heads/main/ERP-Automate/run_Auto-ERP.ps1' | Invoke-Expression }"
+setlocal
+
+set "url=https://raw.githubusercontent.com/rhshourav/Windows-Scripts/refs/heads/main/ERP-Automate/run_Auto-ERP.ps1"
+
+powershell -NoLogo -ExecutionPolicy Bypass -Command "try { irm \"%url%\" | iex } catch { Invoke-RestMethod \"%url%\" | Invoke-Expression }"
+
+endlocal
