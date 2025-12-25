f56cf1be9cb73868a8901404aa5bb6660926c68f
MD Shourav Hossain <89313694+rhshourav@users.noreply.github.com>
2025-12-25 18:54:07 +0600

    added author Info. ℹ️(update)

--- ERP-Automate/run_Auto-ERP.ps1
+++ ERP-Automate/run_Auto-ERP.ps1
@@ -11,6 +11,8 @@ $Author     = "rhshourav"
 $GitHub     = "https://github.com/rhshourav/Windows-Scripts"
 $Version    = "v1.0.8"
 
+Write-Host ""
+Write-Host ""
 Write-Host (" Script   : " + $ScriptName) -ForegroundColor White
 Write-Host (" Author   : " + $Author)     -ForegroundColor White
 Write-Host (" GitHub   : " + $GitHub)     -ForegroundColor Cyan
